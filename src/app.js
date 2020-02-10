b399fb85e1c6660c0f2d79d1134323e6428aec1a
mandoo180 <mandoo180@gmail.com>
2020-02-10 21:36:52 +0900

    third

--- src/app.js
+++ src/app.js
@@ -5,21 +5,8 @@ import './styles/styles.scss';
 import 'normalize.css/normalize.css';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
 
 const store = configureStore();
-
-store.dispatch(
-  addExpense({ description: 'water bill', amount: 300000000, createdAt: 300 })
-);
-store.dispatch(addExpense({ description: 'gas bill', amount: 500, createdAt: 500 }));
-store.dispatch(addExpense({ description: 'rent', amount: 49000000, createdAt: 1000 }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
